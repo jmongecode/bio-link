d6037f3166faf424ea2b29d01b487230e0ebcb0b
Jesús Monge <109181146+jmongecode@users.noreply.github.com>
2024-05-17 10:18:02 -0500

    Initial commit

---
+++ src/components/DevelopedBy.tsx
@@ -0,0 +1,22 @@
+// TODO : update developer
+
+const DevelopedBy = () => {
+  return (
+    <div className="mb-8 font-mono text-neutral-10 text-xs text-center">
+      <span>
+        Developed by{" "}
+        <a
+          href="https://github.com/flamrdevs"
+          className="inline-block transition duration-300 hover:-translate-y-px outline-none hover:text-primary-10 focus-visible:text-primary-10"
+          target="_blank"
+          aria-label="Developer"
+        >
+          {" "}
+          flamrdevs
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default DevelopedBy;
