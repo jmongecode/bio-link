03d7f047a2a9d34cd6d88e8392af7bb4e27be17d
Jesùs Monge <jesus_jaml@outlook.com>
2024-05-31 02:50:08 -0500

    refactor: Add showHeader prop to MainLayout component

--- src/components/Ebook.tsx
+++ src/components/Ebook.tsx
@@ -1,26 +1,28 @@
+import { Simple } from "~/components/icons/index.ts";
+import { Button } from "~/components/ui/index.ts";
 
 function Ebook() {
 
     /** submit form to /api/ebook */
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const form = e.target as HTMLFormElement;
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries());
-        const response = await fetch('/api/ebook', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-        if (response.ok) {
-           // Property 'reset' does not exist on type 'EventTarget'
-            form.reset();
-            alert('Gracias por suscribirte a nuestro boletín');
-        }
-    }
+    // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     const form = e.target as HTMLFormElement;
+    //     const formData = new FormData(form);
+    //     const data = Object.fromEntries(formData.entries());
+    //     const response = await fetch('/api/ebook', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(data),
+    //     });
+    //     if (response.ok) {
+    //        // Property 'reset' does not exist on type 'EventTarget'
+    //         form.reset();
+    //         alert('Gracias por suscribirte a nuestro boletín');
+    //     }
+    // }
 
   return (
 <section className="">
@@ -28,11 +30,12 @@ function Ebook() {
       <div className="mx-auto max-w-screen-md sm:text-center">
 
             <div className="mb-8">
-                <img src="/mockup-ebook.png" alt="Ebook" className="mx-auto w-72 sm:w-96" />
+                <img src="/ebook.png" alt="Ebook" className="p-4 md:p-0 mx-auto md:h-96" />
+                {/* <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Bienvenido a 'Los Fundamentos de la Creación Personal'. Este ebook es tu guía para entender y aplicar los tres principios esenciales para transformar tu vida: Espíritu, Mente y Cuerpo.</p> */}
             </div>
-          <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Bienvenido a 'Los Fundamentos de la Creación Personal'. Este ebook es tu guía para entender y aplicar los tres principios esenciales para transformar tu vida: Espíritu, Mente y Cuerpo.</p>
-          <form onSubmit={handleSubmit} className="">
-              <div className="flex-col gap-2 items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+          <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Ingresa a nuestro Canal de Telegram para Descargar el Ebook <span className="font-bold text-primary" >"Los Fundamentos de la Creación Personal"</span> y comienza a transformar tu vida hoy mismo.</p>
+          {/* <form onSubmit={handleSubmit} className=""> */}
+              {/* <div className="flex-col gap-2 items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                   <div className=" relative w-full">
                       <label htmlFor="name" className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
                       <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -47,12 +50,19 @@ function Ebook() {
                       </div>
                       <input className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Tú mejor correo" type="email" id="email" required/>
                   </div>
-              </div>
+              </div> */}
                 <div className="mb-2 md:mb-4">
-                  <button type="submit" className="py-3 px-5 w-full text-sm font-black text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">¡Obtener Ahora!</button>
+                  {/* <button type="submit" className="transition duration-500 hover:scale-105 py-3 px-5 w-full text-sm font-black text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600  dark:focus:ring-primary-800">¡Obtener Ahora!</button> */}
+                  {/* <button  className="before:ease relative h-12 w-40 overflow-hidden border border-green-500 bg-green-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40">
+      <a href="https://t.me/+VrBS4Z4tJRc3ZWQ5" target="_blank" rel="noreferer noopener" className="relative z-10"> ¡DESCARGAR PDF! </a>
+    </button> */}
                 </div>
-              <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">Nos comprometemos a proteger tu privacidad. Nunca compartiremos tu información.. <a href="#" className="font-medium text-primary-600 dark:text-primary-500 hover:underline">Lee nuestra política de privacidad</a>.</div>
-          </form>
+                <Button as="a" href="https://t.me/+VrBS4Z4tJRc3ZWQ5" target="_blank" aria-label="Unete al grupo de telegram" title="¡DESCARGAR PDF!">
+          <Simple.IconTelegram />
+          ¡DESCARGAR PDF!
+        </Button>
+              {/* <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">Nos comprometemos a proteger tu privacidad. Nunca compartiremos tu información.. <a href="#" className="font-medium text-primary-600 dark:text-primary-500 hover:underline">Lee nuestra política de privacidad</a>.</div> */}
+          {/* </form> */}
       </div>
   </div>
 </section>
